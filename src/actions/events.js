e8b5c7f9d3cb52f0d333dfc193779450e3968826
Avenger7x <avenger7x13@gmail.com>
2015-02-08 16:57:53 +0200

    delete old stuff

--- src/actions/events.js
+++
@@ -1,9 +0,0 @@
-var EventsStore = require('../stores/events');
-
-exports.destroy = function(id){
-	EventsStore.destroy(id);
-};
-
-exports.update = function(id, data){
-	EventsStore.update(id, data);
-};
