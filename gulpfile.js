36f3cdb8fa92beb9958d10356c6a29663e8868d3
Avenger7x <avenger7x13@gmail.com>
2015-02-11 17:06:34 +0200

    clean task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp'),
 	browserify = require('gulp-browserify');
 
-gulp.task('build', function(){
-	return gulp.src('./src/index.js')
+gulp.task('clean', function(){
+	return gulp.src('./app/dist', { read: false })
+		.pipe(rimraf());
+});
+
+gulp.task('build', ['clean'], function(){
+	return gulp.src('./app/src/index.js')
 		.pipe(browserify({ transform: 'reactify' }))
-		.pipe(gulp.dest('./dist'));
+		.pipe(gulp.dest('./app/dist'));
 });
 
 gulp.task('default', ['build']);
