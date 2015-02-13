9b5a9ab8c069b871c111a7652829c2eab3832c6c
Avenger7x <avenger7x13@gmail.com>
2015-02-13 18:16:31 +0200

    uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 	rimraf = require('gulp-rimraf'),
+	uglify = require('gulp-uglify'),
 	browserify = require('gulp-browserify');
 
 gulp.task('clean', function(){
@@ -10,6 +11,7 @@ gulp.task('clean', function(){
 gulp.task('build', ['clean'], function(){
 	return gulp.src('./app/src/index.js')
 		.pipe(browserify({ transform: 'reactify' }))
+		.pipe(uglify())
 		.pipe(gulp.dest('./app/dist'));
 });
 
